6004adeecdb3aa597e64d689c2a2ed833c916302
Elmatsidis Paul <p.elmatsidis@gmail.com>
2020-03-03 15:38:26 +0200

    Implemented firebase utils, user auth storing

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-import "./App.css";
+import './App.css';
 
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import { auth } from './firebase/firebase.utils'
-
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
 
         this.state = {
             currentUser: null
-        }
+        };
     }
 
-    unsubscribeFromAuth = null
+    unsubscribeFromAuth = null;
 
     componentDidMount() {
-        auth.onAuthStateChanged(user=> {
-            this.setState({ currentUser: user})
-        })
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth) {
+                const userRef = await createUserProfileDocument(userAuth);
+
+
+                userRef && userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    });
+
+                    console.log(this.state);
+                });
+            }
+
+            this.setState({ currentUser: userAuth });
+        });
     }
 
     componentWillUnmount() {
@@ -34,11 +49,11 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header currentUser={ this.state.currentUser }/>
+                <Header currentUser={this.state.currentUser} />
                 <Switch>
-                    <Route exact path="/" component={HomePage}/>
-                    <Route path="/shop" component={ShopPage}/>
-                    <Route path="/signin" component={SignInAndSignUpPage}/>
+                    <Route exact path='/' component={HomePage} />
+                    <Route path='/shop' component={ShopPage} />
+                    <Route path='/signin' component={SignInAndSignUpPage} />
                 </Switch>
             </div>
         );
